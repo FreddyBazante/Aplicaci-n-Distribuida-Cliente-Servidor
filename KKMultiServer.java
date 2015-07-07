b0590124f511bfc03a4b61e9adc63ddc48d7380f
FreddyBazante <vovin_therion@yahoo.com>
2015-07-07 02:02:20 -0500

    Create KKMultiServer.java

---
+++ KKMultiServer.java
@@ -0,0 +1,22 @@
+import java.net.*;
+import java.io.*;
+
+public class KKMultiServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        boolean listening = true;
+
+        try {
+            serverSocket = new ServerSocket(4444);
+			System.out.println("Socket escuchando en puerto: "+serverSocket.toString);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 4444.");
+            System.exit(-1);
+        }
+
+        while (listening)
+	    new KKMultiServerThread(serverSocket.accept()).start();
+		 
+        serverSocket.close();
+    }
+}
